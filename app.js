2753c98598703bcc37baf042ddf762638f43ca50
Muhammad Ashar <ashariqbal947@gmail.com>
2024-04-04 08:11:44 +0500

    changes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express()
 const stripe = require('stripe')('sk_test_51P0w1dClp5yFoXKbqGSszYXJQv763m9351pW4agVo2MaVSgsaT2iMR1K8VDtOr66AI0Pm2JDp38cgXo4U3ktOaaT00M6LhfFcW');
 app.use(express.json())
+const payment_method = "pm_1P1eS9Clp5yFoXKb7G9uyaFd"
+const customer_id = "cus_Pqdj1Qe1iZyE5a"
+const axios = require('axios');
 
 
 app.get('/', (req, res) => {
@@ -53,12 +56,57 @@ app.post('/customer/card/:id', async (req, res) => {
 
 })
 
+app.get('/customer/all/cards', async (req, res) => {
+    const paymentMethods = await stripe.paymentMethods.list({
+        customer: customer_id,
+        type: 'card',
+    });
+    return res.status(200).json({ data: paymentMethods })
+
+
+    const stripeSecretKey = 'sk_test_51P0w1dClp5yFoXKbqGSszYXJQv763m9351pW4agVo2MaVSgsaT2iMR1K8VDtOr66AI0Pm2JDp38cgXo4U3ktOaaT00M6LhfFcW';
+
+    await axios.get(`https://api.stripe.com/v1/customers/${customer_id}/cards`, {
+        params: {
+            limit: 3
+        },
+        headers: {
+            'Authorization': `Bearer ${stripeSecretKey}`
+        }
+    })
+        .then(response => {
+            return res.status(200).json({ data: response.data })
+        })
+        .catch(error => {
+            return res.status(400).json({ error })
+
+        });
+
+})
+
+app.post('/create/customer/card', async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            mode: 'setup',
+            currency: 'usd',
+            customer: customer_id,
+            success_url: 'http://localhost:5000/success?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url: 'https://localhost:5000/cancel',
+        });
+        return res.status(200).json({ id: session.id })
+
+    } catch (error) {
+        return res.status(400).json({ error })
+    }
+})
+
+
 
 app.post('/create-payment-stripe', async (req, res) => {
     try {
         const words = 1000
         const price = 100
-        console.log("here")
+        console.log("here2")
         const amountInCents = price * 100;
         // const session = await stripe.checkout.sessions.create({
         //     line_items: [
@@ -73,12 +121,23 @@ app.post('/create-payment-stripe', async (req, res) => {
         //             quantity: 1,
         //         },
         //     ],
-        //     setup_future_usage: "off_session",
+        //     mode: 'payment',
+        //     payment_method_types: ['card'],
         //     customer: "cus_Pqdj1Qe1iZyE5a",
+        //     // setup_future_usage: 'on_session',
         //     mode: "payment",
         //     success_url: "http://localhost:5000/success2",
         //     cancel_url: "http://localhost:5000/cancel",
         // })
+
+        // const session = await stripe.checkout.sessions.create({
+        //     mode: 'setup',
+        //     currency: 'usd',
+        //     customer: 'cus_Pqdj1Qe1iZyE5a',
+        //     success_url: 'http://localhost:5000/success?session_id={CHECKOUT_SESSION_ID}',
+        //     cancel_url: 'https://localhost:5000/cancel',
+        // });
+
         const paymentIntent = await stripe.paymentIntents.create
             ({
                 customer
@@ -90,11 +149,53 @@ app.post('/create-payment-stripe', async (req, res) => {
                 currency
                     : 'usd',
             });
-            console.log(paymentIntent)
+        console.log(paymentIntent.client_secret)
         return res.status(200).json({ id: paymentIntent.client_secret })
     } catch (error) {
-        console.error(error); // Log the error for debugging
+        console.error(error, "error"); // Log the error for debugging
         return res.status(500).json({ message: "Failed" }) // Return a 500 status code for server errors
+    }
+})
+
+
+app.get('/success', async (req, res) => {
+    try {
+        const session_id = req.query.session_id;
+        const session = await stripe.checkout.sessions.retrieve(session_id);
+        // return res.status(200).json({ message: "Done",  session, session_id })
+
+        const setupIntent_id = await session.setup_intent
+        const setupIntent = await stripe.setupIntents.retrieve(setupIntent_id);
+        return res.status(200).json({ payment_method: setupIntent.payment_method, message: "Done", setupIntent, session, session_id })
+    } catch (error) {
+        return res.status(400).json({ error: error.message })
+    }
+})
+
+app.get('/cancel', async (req, res) => {
+    return res.status(200).send("Cancelled")
+})
+
+
+app.post('/charge/customer', async (req, res) => {
+    try {
+        console.log("/charge/customer")
+        const paymentIntent = await stripe.paymentIntents.create({
+            customer: customer_id,
+            payment_method: payment_method,
+            amount: 10 * 100,
+            currency: 'usd',
+            automatic_payment_methods: {
+                enabled: true,
+                allow_redirects : 'never'
+            },
+            confirm: true,
+            receipt_email : "ashariqbal947@gmail.com"
+        });
+        return res.status(200).json({ paymentIntent })
+
+    } catch (error) {
+        res.status(400).json({ error })
     }
 })
 
